3cd7b1c1fc0327fc4ca46ec3d815b02d6005316d
tongsky <416178264@qq.com>
2019-04-15 19:47:01 +0800

    Upload source code
    Upload source code to Github

---
+++ include/ls01b_v2/ls01b.h
@@ -0,0 +1,176 @@
+/*******************************************************
+@company: Copyright (C) 2018, Leishen Intelligent System
+@product: LS01B
+@filename: ls01b.h
+@brief:
+@version:       date:       author:     comments:
+@v1.0           18-8-21     fu          new
+*******************************************************/
+#ifndef LS01B_H
+#define LS01B_H
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread.hpp>
+#include <string>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include "ls01b_v2/lsiosr.h"
+#include "ls01b_v2/resolution.h"
+
+namespace ls {
+
+typedef struct {
+    double degree;
+    double range;
+    double intensity;
+} ScanPoint;
+
+//typedef struct {
+//  std::chrono::steady_clock::time_point time_stamp;
+//  std::vector<ScanPoint> points;
+//}ScanMsg;
+
+class LS01B
+{
+public:
+  LS01B();
+  ~LS01B();
+    /**
+    * 实例化雷达
+    * port: 串口号，
+    * baud_rate: 波特率 460800
+    * resolution: 雷达角度分辨率1.0度 0.5度 0.25度
+    */
+    static LS01B* instance();
+
+    /**
+    * 判断雷达是否有效
+    */
+    bool isHealth();
+
+    /**
+     * 恢复雷达状态
+     */
+    bool resetHealth();
+
+    /**
+    * 获取雷达数据
+    * poins: 雷达点的数据。类型为ScanPoint数组
+    */
+    int getScan(std::vector<ScanPoint> &points, ros::Time &scan_time, float &scan_duration);
+
+    /**
+    * 获取软件版本号
+    * version: 返回版本号
+    */
+    int getVersion(std::string &version);
+
+
+    int getRate();
+
+    /**
+     * start lidar, System work normally
+     */
+    int startScan();
+
+    /**
+     * stop lidar, System in sleep mode
+     */
+    int stopScan();
+
+    /**
+     * set scan mode. continouse or once
+     * is_continuous:
+     */
+    int setScanMode(bool is_continuous);
+
+    /**
+     * stop recv data from lidar
+     */
+    int stopRecvData();
+
+    /**
+     * switch angle or intensity in recv data
+     * use_angle: true. the angle is chosed
+     *            false. the intensity is chosed
+     */
+    int switchData(bool use_angle);
+
+    /**
+     * set the motor spped
+     * rpm: round per minute
+     */
+    int setMotorSpeed(int rpm);
+
+    /**
+    * 设置雷达数据分辨率
+    * resolution: 分辨率
+    */
+    int setResolution(double resolution);
+
+    double getRPM();
+
+    void run();
+
+    bool resServerCallback(ls01b_v2::resolution::Request &req,
+                   ls01b_v2::resolution::Response &res);
+
+  private:
+    void initParam();
+    void recvThread();
+    void pubScanThread();
+    
+
+    uint16_t checkSum(const uint8_t *p_byte);
+
+    std::vector<ScanPoint> scan_points_;
+    std::vector<ScanPoint> scan_points_bak_;
+//    ScanMsg scan_msg_;
+
+    LSIOSR * serial_;
+    boost::thread *recv_thread_;
+    boost::thread *pubscan_thread_;
+    boost::mutex pubscan_mutex_;
+    boost::mutex mutex_;
+    boost::condition_variable pubscan_cond_;
+    ros::ServiceServer changeRes_client;
+
+    bool is_shutdown_;    // shutdown recvthread
+    bool is_start_;       // begin to read data from lidar
+    bool use_angle_;
+    
+    int scan_health_; // 0 OK
+    double resolution_;
+    int data_len_;
+
+    int points_size_;
+    int rpm_;
+    double real_rpm_;
+
+    ros::Time pre_time_;
+    ros::Time time_;
+
+    std::string serial_port_;
+    int baud_rate_;
+    std::string scan_topic_;
+    std::string frame_id_;
+    double robot_radius_;
+    double center_x_;
+    double center_y_;
+    double rmp_;
+    double angle_disable_min_0;
+    double angle_disable_max_0;
+    double angle_disable_min_1;
+    double angle_disable_max_1;
+    double angle_disable_min_2;
+    double angle_disable_max_2;
+    double angle_disable_min_3;
+    double angle_disable_max_3;
+    double angle_disable_min_4;
+    double angle_disable_max_4;
+
+    ros::NodeHandle n_;
+    ros::Publisher pub_;
+};
+
+}
+#endif
---
+++ src/ls01b.cpp
@@ -0,0 +1,554 @@
+/*******************************************************
+@company: Copyright (C) 2018, Leishen Intelligent System
+@product: LS01B
+@filename: ls01b.cpp
+@brief:
+@version:       date:       author:     comments:
+@v1.0           18-8-21     fu          new
+*******************************************************/
+#include "ls01b_v2/ls01b.h"
+#include <stdio.h>
+#include <signal.h>
+
+namespace ls{
+LS01B * LS01B::instance()
+{
+  static LS01B obj;
+  return &obj;
+}
+
+LS01B::LS01B()
+{
+  initParam();
+  pub_ = n_.advertise<sensor_msgs::LaserScan>(scan_topic_, 1000);
+  serial_ = LSIOSR::instance(serial_port_, baud_rate_);
+  serial_->init();
+  changeRes_client = n_.advertiseService("resolution_control", &LS01B::resServerCallback, this);
+  recv_thread_ = new boost::thread(boost::bind(&LS01B::recvThread, this));
+  pubscan_thread_ = new boost::thread(boost::bind(&LS01B::pubScanThread, this));
+}
+
+LS01B::~LS01B()
+{
+  printf("start LS01B::~LS01B()\n");
+  stopScan();
+  is_shutdown_ = true;
+
+  pubscan_thread_->interrupt();
+  pubscan_thread_->join();
+  pubscan_thread_ = NULL;
+  delete pubscan_thread_;
+
+  recv_thread_->interrupt();
+  recv_thread_->join();
+
+  recv_thread_ = NULL;
+  delete recv_thread_;
+
+  serial_->close();
+  serial_ = NULL;
+  delete serial_;
+  printf("end LS01B::~LS01B()\n");
+}
+void LS01B::initParam()
+{
+  std::string scan_topic = "/scan";
+  std::string frame_id = "laser_link";
+  std::string port = "/dev/ttyUSB0";
+  ros::NodeHandle nh("~");
+  nh.param("scan_topic", scan_topic_, scan_topic);
+  nh.param("frame_id", frame_id_, frame_id);
+  nh.param("serial_port", serial_port_, port);
+  nh.param("baud_rate", baud_rate_, 460800);
+  nh.param("angle_resolution", resolution_, 1.0);
+  nh.param("angle_disable_min_0", angle_disable_min_0, -1.0);
+  nh.param("angle_disable_max_0", angle_disable_max_0, -1.0);
+  nh.param("angle_disable_min_1", angle_disable_min_1, -1.0);
+  nh.param("angle_disable_max_1", angle_disable_max_1, -1.0);
+  nh.param("angle_disable_min_2", angle_disable_min_2, -1.0);
+  nh.param("angle_disable_max_2", angle_disable_max_2, -1.0);
+  nh.param("angle_disable_min_3", angle_disable_min_3, -1.0);
+  nh.param("angle_disable_max_3", angle_disable_max_3, -1.0);
+  nh.param("angle_disable_min_4", angle_disable_min_4, -1.0);
+  nh.param("angle_disable_max_4", angle_disable_max_4, -1.0);
+  nh.param("robot_radius", robot_radius_, 0.2);
+  nh.param("center_x", center_x_, 0.0);
+  nh.param("center_y", center_y_, 0.0);
+  nh.param("rpm", rpm_, 600);
+
+  scan_health_ = 0;
+  is_shutdown_ = false;
+  is_start_ = false;
+  use_angle_ = true;
+
+  data_len_ = 180;
+  points_size_ = 360 / resolution_;
+  scan_points_.resize(points_size_);
+}
+
+bool LS01B::isHealth()
+{
+  return (scan_health_ < 0) ? false : true;
+}
+
+bool LS01B::resetHealth()
+{
+  scan_health_ = 0;
+}
+
+int LS01B::getScan(std::vector<ScanPoint> &points, ros::Time &scan_time, float &scan_duration)
+{
+  // boost::unique_lock<boost::mutex> lock(mutex_);
+  points.assign(scan_points_bak_.begin(), scan_points_bak_.end());
+  scan_time = pre_time_;
+
+  scan_duration = (time_ - pre_time_).toSec();
+  // ROS_INFO("scan_duration = %f", scan_duration);
+}
+
+int LS01B::getVersion(std::string &version)
+{
+  version = "ls01b_v2_0";
+  return 0;
+}
+
+double LS01B::getRPM()
+{
+  return real_rpm_;
+}
+
+int LS01B::startScan()
+{
+  printf("startScan\n");
+  char data[3];
+  data[0] = 0xa5;
+  data[1] = 0x2c;
+  data[2] = 0x0;
+  int rtn = serial_->send((const char*)data, 2);
+  if (rtn < 0)
+  {
+    printf("start scan error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+
+  return rtn;
+}
+
+int LS01B::stopScan()
+{
+  printf("stopScan\n");
+  char data[2];
+  data[0] = 0xa5;
+  data[1] = 0x25;
+  int rtn = serial_->send((const char *)data, 2);
+  if (rtn < 0)
+  {
+    printf("stop scan error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+  is_start_ = false;
+  return rtn;
+}
+
+int LS01B::setScanMode(bool is_continuous)
+{
+  printf("setScanMode is_continuous = %d\n", is_continuous);
+  char data[2];
+  data[0] = 0xa5;
+  data[1] = 0x0;
+  
+  if (is_continuous){
+    data[1] = 0x20;
+  }
+  else{
+    data[1] = 0x22;
+  }
+
+  int rtn = serial_->send((const char *)data, 2);
+  if (rtn < 0)
+  {
+    printf("setScanMode error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+  is_start_ = true;
+  return rtn;
+}
+
+int LS01B::stopRecvData()
+{
+  printf("stopRecvData\n");
+  char data[2];
+  data[0] = 0xa5;
+  data[1] = 0x21;
+
+  int rtn = serial_->send((const char *)data, 2);
+  if (rtn < 0)
+  {
+    printf("stopRecvData error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+  is_start_ = false;
+  return rtn;
+}
+
+int LS01B::switchData(bool use_angle)
+{
+  printf("switchData use_angle = %d\n", use_angle);
+  char data[4];
+  data[0] =  0xa5;
+  data[1] = 0x0;
+  data[2] = 0x0;
+  data[3] = 0x0;
+
+  if (use_angle){
+    data[1] = 0x5c;
+  }
+  else{
+    data[1] = 0x50;
+  }
+
+  int rtn = serial_->send((const char *)data, 4);
+  if (rtn < 0)
+  {
+    printf("switchData error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+  is_start_ = false;
+  use_angle_ = use_angle;
+
+  return rtn;
+}
+
+int LS01B::setMotorSpeed(int rpm)
+{
+  printf("setMotorSpeed rpm = %d\n", rpm);
+
+  char data[4];
+  data[0] = 0xa5;
+  data[1] = 0x26;
+  data[3] = (rpm & 0xff);
+  data[2] = (rpm >> 8) & 0xff;
+  // printf("0x%x, 0x%x\n", data[2], data[3]);
+  int rtn = serial_->send((const char *)data, 4);
+  if (rtn < 0)
+  {
+    printf("setMotorSpeed error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+  is_start_ = false;
+
+  return rtn;
+}
+
+int LS01B::setResolution(double resolution)
+{
+  printf("setResolution resolution = %f\n", resolution);
+  char data[4];
+  data[0] = 0xa5;
+  data[1] = 0x30;
+  data[2] = 0x00;
+
+  if (resolution == 0.25){
+    data[3] = 0x19;
+    data_len_ = 180;
+  }
+  else if (resolution == 0.5){
+    data[3] = 0x32;
+    data_len_ = 90;
+  }
+  else if (resolution == 1.0){
+    data[3] = 0x64;
+    data_len_ = 45;
+  }
+  else
+  {
+    // resolution error
+    scan_health_ = -2;
+    return scan_health_;
+  }
+
+  resolution_ = resolution;
+  points_size_ = 360/resolution;
+  scan_points_.resize(points_size_);
+  
+
+  int rtn = serial_->send((const char *)data, 4);
+  if (rtn < 0)
+  {
+    printf("setResolution error !\n");
+    scan_health_ = -1;
+    return rtn;
+  }
+  is_start_ = false;
+
+  return rtn;
+}
+
+bool LS01B::resServerCallback(ls01b_v2::resolution::Request &req,
+                   ls01b_v2::resolution::Response &res)
+{
+  double resolution = req.resolution.data;
+  if ((resolution == 0.25) | (resolution == 0.5)  | (resolution == 1.0))
+  {
+    ROS_WARN("User want to switch to resolution %1.2f degree", resolution);
+  }
+  else{
+    ROS_WARN("Invalid angle resolution to set! Keep it same");
+    res.status = false;
+    return true;
+  }
+  stopRecvData();   // stop sending data
+  usleep(100);
+  setResolution(resolution);
+  usleep(100);
+  setScanMode(true);
+  res.status = true;
+  return true;
+}
+void LS01B::recvThread()
+{
+
+  uint8_t start_count = 0;
+  char header[6];
+  uint8_t temp_char;
+  char * packet_bytes = new char[data_len_];
+  if (packet_bytes == NULL)
+  {
+    packet_bytes = NULL;
+    // printf("new char [data_len_] error \n");
+  }
+
+  boost::posix_time::ptime t1,t2;
+  t1 = boost::posix_time::microsec_clock::universal_time();
+  while(!is_shutdown_&&ros::ok()){
+    while(!is_start_){
+      usleep(300000);
+    }
+
+    bool is_health = this->isHealth();
+    if (!is_health)
+    {
+      this->resetHealth();
+      serial_ = LSIOSR::instance(serial_port_, baud_rate_);
+      run();
+      continue;
+    }
+
+    int count = serial_->read(&header[start_count], 1);
+    if (count<=0)
+    {
+      // printf("read header[%d] error\n", start_count);
+      start_count = 0;
+      scan_health_ = -3;
+      continue;
+    }
+
+    if(0 == start_count){
+      if (0xA5 == (header[start_count]&0xff)){
+        start_count = 1;
+      }
+      else{
+        start_count = 0;
+      }
+    }
+    else if (1 == start_count) {
+      if (0x6A == (header[start_count]&0xff) || 0x5A == (header[start_count]&0xff))
+      {
+        if (0x6A == (header[start_count]&0xff))
+        {
+          t2 = boost::posix_time::microsec_clock::universal_time();
+          boost::posix_time::millisec_posix_time_system_config::time_duration_type t_elapse;
+          t_elapse = t2 - t1;
+          real_rpm_ = 1000000.0 / t_elapse.ticks();
+
+          t1 = t2;
+
+          boost::unique_lock<boost::mutex> lock(mutex_);
+          scan_points_bak_.resize(scan_points_.size());
+          scan_points_bak_.assign(scan_points_.begin(), scan_points_.end());
+          pre_time_ = time_;
+          lock.unlock();
+          
+          pubscan_cond_.notify_one();
+          time_ = ros::Time::now();
+        }
+
+        start_count = 2;
+        int count = serial_->read(&header[start_count], 4);
+        if (count != 4)
+        {
+          // printf("read header error\n");
+          start_count = 0;
+          scan_health_ = -3;
+          continue;
+        }
+
+        rpm_ = ((header[2]&0x7f) << 8) + (header[3]&0xff);
+        int flag = ((header[2] & 0x80) == 0) ? 0 : 1;
+        int angular_resolution = (header[4]&0xff) >> 1;
+        int start_angle = ((header[4] & 0x01) << 8) + (header[5]&0xff);
+
+        count = serial_->read(packet_bytes, data_len_);
+        if (count != data_len_)
+        {
+          // printf("read %d packet error\n", data_len_);
+          start_count = 0;
+          scan_health_ = -3;
+          continue;
+        }
+
+        for (int i = 0; i < data_len_; i = i+3)
+        {
+          // printf("%02X %02X %02X \n", (packet_bytes[i]) & 0xFF, (packet_bytes[i+1]) & 0xFF, (packet_bytes[i+2]) & 0xFF);
+          int idx = start_angle / resolution_ + i/3;
+          double distance = ((packet_bytes[i+1] & 0xFF) << 8) | (packet_bytes[i+2] & 0xFF);         
+          // double degree = start_angle + (packet_bytes[i] & 0xFF )* resolution_;
+          double degree = start_angle + idx * resolution_;
+
+          boost::unique_lock<boost::mutex> lock(mutex_);
+          scan_points_[idx].degree = degree;
+          scan_points_[idx].range = distance/1000.0;
+          if(use_angle_)
+          {
+            scan_points_[idx].intensity = 0;
+          }
+          else
+          {
+            scan_points_[idx].intensity = packet_bytes[i] & 0xFF;
+          }
+          lock.unlock();
+        }
+
+        start_count = 0;
+
+      }
+      else{
+        start_count = 0;
+      }
+
+    }
+  }
+
+  if (packet_bytes)
+  {
+    packet_bytes = NULL;
+    delete packet_bytes;
+  }
+}
+
+void LS01B::pubScanThread()
+{
+  bool wait_for_wake = true;
+  boost::unique_lock<boost::mutex> lock(pubscan_mutex_);
+
+  while (ros::ok() && !is_shutdown_)
+  {
+    while (wait_for_wake)
+    {
+      // ROS_WARN("pubScanThread thread is suspending");
+      pubscan_cond_.wait(lock);
+      wait_for_wake = false;
+    }
+
+    std::vector<ScanPoint> points;
+    ros::Time start_time;
+    float scan_time;
+    this->getScan(points, start_time, scan_time);
+    int count = points.size();
+    if (count <= 0)
+      continue;
+
+    sensor_msgs::LaserScan msg;
+    msg.header.frame_id = frame_id_;
+    msg.header.stamp = start_time;
+    msg.angle_min = 0.0;
+    msg.angle_max = 2 * M_PI;
+    msg.angle_increment = (msg.angle_max - msg.angle_min) / count;
+    msg.range_min = 0.01;
+    msg.range_max = 25;
+    msg.ranges.resize(count);
+    msg.intensities.resize(count);
+    msg.scan_time = scan_time;
+    msg.time_increment = scan_time / (double)(count - 1);
+
+    for (int i = count - 1; i >= 0; i--)
+    {
+      if (
+          ((i >= (angle_disable_min_0 * count / 360)) && (i < (angle_disable_max_0 * count / 360))) 
+          || ((i >= (angle_disable_min_1 * count / 360)) && (i < (angle_disable_max_1 * count / 360))) 
+          || ((i >= (angle_disable_min_2 * count / 360)) && (i < (angle_disable_max_2 * count / 360))) 
+          || ((i >= (angle_disable_min_3 * count / 360)) && (i < (angle_disable_max_3 * count / 360))) 
+          || ((i >= (angle_disable_min_4 * count / 360)) && (i < (angle_disable_max_4 * count / 360))))
+      {
+        msg.ranges[i] = std::numeric_limits<float>::infinity();
+        msg.intensities[i] = 0;
+      }
+      else if (points[count - i - 1].range == 0.0)
+      {
+        msg.ranges[i] = std::numeric_limits<float>::infinity();
+        msg.intensities[i] = 0;
+      }
+      else
+      {
+        msg.ranges[i] = (float)points[count - i - 1].range;
+        msg.intensities[i] = points[count - i - 1].intensity;
+      }
+
+      double point_dist = msg.ranges[i];
+      if (point_dist < 1.0 && point_dist > 0.06)
+      {
+        double x = point_dist * cos(i * resolution_ * M_PI / 180);
+        double y = point_dist * sin(i * resolution_ * M_PI / 180);
+
+        double dist2center = sqrt((y - center_y_) * (y - center_y_) + (x - center_x_) * (x - center_x_));
+        if (dist2center < robot_radius_)
+          msg.ranges[i] = std::numeric_limits<float>::infinity();
+      }
+    }
+    pub_.publish(msg);
+    // ROS_INFO("getRPM = %f", getRPM());
+    wait_for_wake = true;
+  }
+}
+
+void LS01B::run()
+{
+  sleep(1);
+  setResolution(resolution_);
+  sleep(1);
+  setMotorSpeed(rpm_);
+  sleep(1);
+  switchData(false);
+  sleep(1);
+  startScan();
+  sleep(1);
+  setScanMode(true); 
+}
+
+
+}
+
+void handleSig(int signo)
+{
+  printf("handleSig\n");
+  ros::shutdown();
+  exit(0);
+}
+
+int main(int argv, char **argc)
+{
+  signal(SIGINT, handleSig);
+  signal(SIGTERM, handleSig);
+  ros::init(argv, argc, "ls01b");
+ 
+  ls::LS01B* ls01b = ls::LS01B::instance();
+  ls01b->run();
+  ros::spin();
+  return 0;
+}
